41085615c7d363096eff2912a2a6b0eef4a3613e
Gabriel <gabrifer@gmail.com>
2021-04-08 21:04:28 -0300

    Adicionado primeira tela

---
+++ frontend/alfa-bd/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/header/header.component.js';
+
+import './App.css';
+import MainPage from './pages/main-page/main-page.component.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
