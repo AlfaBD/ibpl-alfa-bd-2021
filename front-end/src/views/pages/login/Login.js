2ded8d60756555babe49f401d123c3c39f6f76c6
Luan Henrique Souza Dantas <luan_h.s.d@hotmail.com>
2021-04-10 22:26:27 -0300

    feat: add front end repository

---
+++ front-end/src/views/pages/login/Login.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { useHistory  } from 'react-router-dom'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CInput,
+  CInputGroup,
+  CInputGroupPrepend,
+  CInputGroupText,
+  CRow
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+
+const Login = () => {
+
+  const history = useHistory();
+
+  const routeChange = () =>{ 
+    let path = '/'; 
+    history.push(path);
+  }
+
+  return (
+    <div className="c-app c-default-layout flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md="8">
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm>
+                    <h1 class="text-primary">ALFA-BD</h1>
+                    <p className="text-muted">Entre com a sua conta</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-user" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput type="text" placeholder="e-mail" autoComplete="username" />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-lock-locked" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput type="password" placeholder="senha" autoComplete="current-password" />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs="6">
+                        <CButton color="secondary" className="px-4">Entrar</CButton>
+                      </CCol>
+                      <CCol xs="6" className="text-right">
+                        <CButton color="link" className="px-0" onClick={routeChange}>Esqueci minha senha</CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Login
