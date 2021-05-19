53c8a39db71fc31a13741f44b354aa4350588518
breslei <bresleimax@rocketmail.com>
2021-05-19 14:19:36 -0300

    uploading dashboard Admin eith buttons...hahahaha

---
+++ front-end/src/views/cadastro/escola/EscolasData.js
@@ -0,0 +1,22 @@
+const usersData = [
+  {id: 0, use_name: 'EMEF Profª Otacília Madureira de Moura', cnpj: '29.868.999/0001-77', codigo: '785696', city: 'São Paulo', state: 'SP'},
+  {id: 1, use_name: 'EMEF Profª Palmyra Sant Anna', cnpj: '29.868.999/0001-77', codigo: '452169', city: 'Guarulhos', state: 'SP'},
+  {id: 2, use_name: 'EMEF Profª Elizabete de Paula Honorato', cnpj: '59.142.669/0001-60', codigo: '012325', city: 'São José dos Campos', state: 'SP'},
+  {id: 3, use_name: 'EMEF Profª Otacília Madureira de Moura', cnpj: '59.142.669/0001-60', codigo: '785120', city: 'Guarulhos', state: 'SP'},
+  {id: 4, use_name: 'EMEF Profª Palmyra Sant Anna Maximinus', cnpj: '26.922.921/0001-03', codigo: '452169', city: 'São Paulo', state: 'SP'},
+  {id: 5, use_name: 'EMEF Profª Elizabete de Paula Honorato Dávid', cnpj: '32.269.115/0001-72', codigo: '012325', city: 'Guarulhos', state: 'SP'},
+  {id: 6, use_name: 'EMEF Profª Otacília Madureira de Moura', cnpj: '29.868.999/0001-77', codigo: '452169', city: 'Guarulhos', state: 'SP'},
+  {id: 7, use_name: 'AvEMEF Profª Palmyra Sant Annaram Tarasios', cnpj: '59.142.669/0001-60', codigo: '012325', city: 'São José dos Campos', state: 'SP'},
+  {id: 8, use_name: 'EMEF Profª Elizabete de Paula Honorato Ed', cnpj: '59.142.669/0001-60', codigo: '785120', city: 'Guarulhos', state: 'SP'},
+  {id: 9, use_name: 'EMEF Profª Otacília Madureira de Moura', cnpj: '26.922.921/0001-03', codigo: '452169', city: 'São Paulo', state: 'SP'},
+  {id: 10, use_name: 'EMEF Profª Palmyra Sant Anna Tadeáš', cnpj: '32.269.115/0001-72', codigo: '012325', city: 'Guarulhos', state: 'SP'},
+  {id: 11, use_name: 'EMEF Profª Elizabete de Paula Honorato Fachtna', cnpj: '29.868.999/0001-77', codigo: '452169', city: 'Guarulhos', state: 'SP'},
+  {id: 12, use_name: 'EMEF Profª Otacília Madureira de Moura', cnpj: '59.142.669/0001-60', codigo: '012325', city: 'São José dos Campos', state: 'SP'},
+  {id: 13, use_name: 'EMEF Profª Palmyra Sant Anna Gemariah', cnpj: '59.142.669/0001-60', codigo: '785120', city: 'Guarulhos', state: 'SP'},
+  {id: 14, use_name: 'EEMEF Profª Elizabete de Paula Honoratoustorgios Amulius', cnpj: '26.922.921/0001-03', codigo: '452169', city: 'São Paulo', state: 'SP'},
+  {id: 15, use_name: 'EMEF Profª Otacília Madureira de Moura', cnpj: '32.269.115/0001-72', codigo: '012325', city: 'Guarulhos', state: 'SP'},
+  {id: 16, use_name: 'EMEF Profª Palmyra Sant Anna René', cnpj: '29.868.999/0001-77', codigo: '452169', city: 'Guarulhos', state: 'SP'},
+  {id: 17, use_name: 'EMEF Profª Elizabete de Paula Honorato Jadon', cnpj: '59.142.669/0001-60', codigo: '012325', city: 'São José dos Campos', state: 'SP'},
+]
+
+export default usersData
