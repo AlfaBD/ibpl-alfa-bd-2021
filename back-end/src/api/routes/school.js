703ad597c60e400b170b164ff87f5dbc7cbcabfa
Gabriel <gabrifer@gmail.com>
2021-05-20 21:44:28 -0300

    Adding authentication routes

--- back-end/src/api/routes/school.js
+++ back-end/src/api/routes/school.js
@@ -1,7 +1,11 @@
 const router = require('express');
 const route = router();
+const tokenAuth = require('../midlewares/token-auth')
 
 module.exports = (app) => {
+
+    // We will keep the token authentication disabled for now
+    //app.use(tokenAuth());
 
     //All routes under this file will be prefixed with /school
     app.use('/school', route);
