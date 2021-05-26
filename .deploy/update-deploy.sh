48c3cc57da98d9928883806dde2a1e3ac4f3da14
AlfaBD <79471409+AlfaBD@users.noreply.github.com>
2021-05-26 18:21:09 -0300

    Update update-deploy.sh

--- .deploy/update-deploy.sh
+++ .deploy/update-deploy.sh
@@ -1,12 +1,12 @@
 
 #!/usr/bin/env bash
 
+git fetch --all
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
-
-git fetch --all
 
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
