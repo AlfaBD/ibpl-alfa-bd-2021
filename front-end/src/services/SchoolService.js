c5a3cb0c3277b635fc240010057c4e131889d856
Gabriel Ferreira <gabrifer@gmail.com>
2021-06-09 23:25:11 -0300

    Adding all the required logic to integrate backend and frontend for entity creations (Students, Teachers & Schools)

--- front-end/src/services/SchoolService.js
+++ front-end/src/services/SchoolService.js
@@ -1,33 +1,32 @@
 import { get, post, update } from "./ApiService";
 
-export default {
-  index() {
-    return get("/school");
-  },
-  store() {
-    return post("/school");
-  },
-  show() {
-    return get("/school");
-  },
-  updateStudent() {
-    return update("/student");
-  },
-  updateTeacher() {
-    return update("/teacher");
-  },
-  updateClass() {
-    return update("/class");
-  },
-  
-  // Endpoints Dahsboard
-  get_classes() {
-    return get("/classes");
-  },
-  get_tasks() {
-    return get("/tasks");
-  },
-  get_evaluations(classes) {
-    return get("/evaluations");
-  }
+
+export async function index() {
+  return get("/school");
+}
+export async function store(data) {
+  return post("/school", data);
+}
+export async function show() {
+  return get("/school");
+}
+export async function updateStudent() {
+  return update("/student");
+}
+export async function updateTeacher() {
+  return update("/teacher");
+}
+export async function updateClass() {
+  return update("/class");
+}
+
+// Endpoints Dahsboard
+export async function get_classes() {
+  return get("/classes");
+}
+export async function get_tasks() {
+  return get("/tasks");
+}
+export async function get_evaluations(classes) {
+  return get("/evaluations");
 }
