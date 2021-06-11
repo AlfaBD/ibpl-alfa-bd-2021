e006c1e89fa7c5181bbe2d68817b126222a22502
Luan Henrique Souza Dantas <luan.dantas@solucx.com.br>
2021-06-11 00:06:10 -0300

    feat: FluencyMetrics

--- back-end/src/controllers/AnalysisController.js
+++ back-end/src/controllers/AnalysisController.js
@@ -1,22 +1,34 @@
-const formidable = require('formidable')
-const form = new formidable.IncomingForm()
-const {PythonShell} = require('python-shell')
+const formidable = require('formidable');
+const form = new formidable.IncomingForm();
+const { PythonShell } = require('python-shell');
+const FluencyMetrics = require('../models/FluencyMetrics');
 
-PythonShell.defaultOptions = { mode: 'text', pythonOptions: ['-u'], scriptPath: './src/controllers/scripts' };
+PythonShell.defaultOptions = {
+  mode: 'text',
+  pythonOptions: ['-u'],
+  scriptPath: './src/controllers/scripts',
+};
 
-const pyshell = new PythonShell('classifier.py')
+const pyshell = new PythonShell('classifier.py');
 
 module.exports = {
-    predict(request, response, fields, files) {
-        pyshell.send(files.file.path);
+  predict(request, response, fields, files) {
+    pyshell.send(files.file.path);
 
-        pyshell.on('message', function (result) {
-            //Todo: persistir os dados no banco
-            return response.json({'status': 'OK', 'result': result});
-        });
+    pyshell.on('message', function (result) {
+      //Todo: persistir os dados no banco
 
-        pyshell.end(function (err, code, signal) {
-            if (err) return response.json({'status': 'ERROR', 'message': err, 'result': null});
-        });
-    },
+      const result = await FluencyMetrics.create({
+        idProfessor: '',
+        idAluno: '',
+        nota: '',
+      });
+      return response.json({ status: 'OK', result: result });
+    });
+
+    pyshell.end(function (err, code, signal) {
+      if (err)
+        return response.json({ status: 'ERROR', message: err, result: null });
+    });
+  },
 };
