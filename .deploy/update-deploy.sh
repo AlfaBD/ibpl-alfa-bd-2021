73919cbeada61b287508e95e261f00b14da63428
Eduardo_Lima <eduardolimacesl@gmail.com>
2021-06-17 15:31:55 -0300

    Inserção do link simbólico pasta audios

--- .deploy/update-deploy.sh
+++ .deploy/update-deploy.sh
@@ -29,4 +29,6 @@ elif [ $LOCAL = $BASE ]; then
     # Deploying new image
     docker run -d -p 80:80 --name=alfabd alfabd:develop
 
+    # Link to audio's folder 
+    ln -s /home/pc06/alfa-bd/audios /home/pc06/ibpl-alfa-bd-2021/ 
 fi
