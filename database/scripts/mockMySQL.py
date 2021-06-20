e491795bb9aa92eda69769a871f0a04292a12252
rodrigues-aline <rodrigues.aline.n@gmail.com>
2021-06-19 22:03:53 -0300

    update script mock

--- database/scripts/mockMySQL.py
+++ database/scripts/mockMySQL.py
@@ -3,13 +3,16 @@
 import getopt
 import sys
 import pymysql
-from faker import Faker
-from random import randint
 
-def debug(texto, *args, **var):
+from random import randint
+from datetime import date, datetime
+from faker import Faker
+from pymongo import MongoClient
+
+def debug(text, *args, **var):
     isdebug = var.get('isdebug', True)
     if isdebug:
-        print("{} {}".format(texto, " ".join([str(x) for x in args])))
+        print('{} {}'.format(text, ' '.join([str(x) for x in args])))
 
 
 class ExecuteMock():
@@ -19,7 +22,8 @@ class ExecuteMock():
         Insert data into MySQL database
     """
 
-    def __init__(self, database="alfabd", host="127.0.0.1", user="root", pw=""):
+    def __init__(self, database='alfabd', host='127.0.0.1', user='root', pw='', url_mongo=''):
+        # mySQL
         self.con = pymysql.connect(host=host, 
                                    user=user, 
                                    password=pw, 
@@ -27,9 +31,17 @@ class ExecuteMock():
                                    cursorclass=pymysql.cursors.DictCursor)
         self.year_birth_student = [2012, 2013]
         self.fake = Faker('pt_BR')
-        self.classes = ['2ºA', '2ºB', '2ºC', '2ºD','2ºE','3ºA', '3ºB', '3ºC', '3ºD', '3ºE']
+        self.classes = ['2ºA', '2ºB', '2ºC', '2ºD','2ºE', '2ºF', '3ºA', '3ºB', '3ºC', '3ºD', '3ºE', '3ºF']
         
-    
+        # mongoDB
+        self.client = MongoClient(url_mongo)
+        self.fluencymetrics = self.client[database]['fluencymetrics']
+        
+        self.metrics = ['alto', 'medio', 'baixo']
+        self.days = [1, 7, 14, 28]
+        self.months = [1, 2, 3, 4, 5, 6]
+        
+            
     def run(self):
         c = self.con.cursor()
         c.execute('DELETE FROM Attendances WHERE att_id > 0;')
@@ -47,35 +59,32 @@ class ExecuteMock():
         c.execute('COMMIT;')
         
         # Insert Schools
-        schools = open("schools.csv", "r")
+        schools = open('schools.csv', 'r')
         school = schools.readline()
         while school != '':
             school.replace('\n', '')
-            query = f"""INSERT INTO Schools (sch_name, sch_cnpj, sch_mecNumber, sch_city, sch_state, sch_status, sch_address, sch_phoneNUmber, sch_email, createdAt, updatedAt) 
-                                     VALUES ('{school}', NULL, NULL, 'São José dos Campos', 'São Paulo', 'Ativo', 'Rua da Alegria, n 42, Jardim das Flores', '1239857458', 'escola@email.com', NOW(), NOW());"""
-            school = schools.readline()
-
+            query = f"""INSERT INTO Schools (createdAt, updatedAt, sch_name, sch_city, sch_state, sch_status, sch_address, sch_phoneNumber, sch_email) 
+                                     VALUES (NOW(), NOW(), '{school}', 'São José dos Campos', 'São Paulo', 'Ativo', 'Rua da Alegria, n 42, Jardim das Flores', '1239857458', 'escola@email.com');"""
             c = self.con.cursor()
             c.execute(query)
+            school = schools.readline()
         
         # Insert Users Students
         for year in self.year_birth_student:
-            for i in range(1, 24001):
+            for i in range(1, 28801):
                 name = self.fake.name()
-                day = randint(1, 28)
-                month = randint(1, 31)
+                day = randint(1, 29)
+                month = randint(1, 13)
                 query = f"""INSERT INTO Users (createdAt, updatedAt, usr_birth_date, usr_email, usr_name, usr_primary_role) 
-                                        VALUES (NOW(), NOW(), '{year}-{month}-{day}', 'student{i}{year}@email.com', '{name}', 'STUDENT');"""
-           
+                                       VALUES (NOW(), NOW(), '{year}-{month}-{day}', 'student{i}{year}@email.com', '{name}', 'STUDENT');"""
                 c = self.con.cursor()
                 c.execute(query)
             
         # Insert Users Teachers
-        for i in range(1, 6001):
+        for i in range(1, 7201):
             name = self.fake.name()
             query = f"""INSERT INTO Users (createdAt, updatedAt, usr_birth_date, usr_email, usr_name, usr_primary_role) 
-                                        VALUES (NOW(), NOW(), '1980-01-01', 'teacher{i}@email.com', '{name}', 'TEACHER');"""
-                                        
+                                   VALUES (NOW(), NOW(), '1980-01-01', 'teacher{i}@email.com', '{name}', 'TEACHER');"""
             c = self.con.cursor()
             c.execute(query) 
                                         
@@ -83,8 +92,7 @@ class ExecuteMock():
         for i in range(1, 121):
             name = self.fake.name()
             query = f"""INSERT INTO Users (createdAt, updatedAt, usr_birth_date, usr_email, usr_name, usr_primary_role) 
-                                        VALUES (NOW(), NOW(), '1980-01-01', 'school{i}@email.com', '{name}', 'SCHOOL');"""
-        
+                                   VALUES (NOW(), NOW(), '1980-01-01', 'school{i}@email.com', '{name}', 'SCHOOL');"""
             c = self.con.cursor()
             c.execute(query) 
         
@@ -94,7 +102,7 @@ class ExecuteMock():
         # Insert Classes
         try:
             with self.con.cursor() as c:
-                c.execute("SELECT sch_id FROM Schools")
+                c.execute('SELECT sch_id FROM Schools;')
                 schools = c.fetchall()
                 c.execute("SELECT usr_id FROM Users WHERE usr_primary_role='TEACHER';")
                 teacher_users = c.fetchall()
@@ -110,11 +118,10 @@ class ExecuteMock():
         for school in schools:
             sch_id = school['sch_id']
             school_user = schools_users[school_index]['usr_id']
-            for i in range(0, 10):
-                cla = self.classes[i]
+            for cla in self.classes:
                 teacher_user = teacher_users[teacher_index]['usr_id']
                 query = f"""INSERT INTO Classes (createdAt, updatedAt, cla_name, cla_status, sch_id, school_user_id, teacher_user_id)
-                                          VALUES (NOW(), NOW(), '{cla}', 'Ativo', {sch_id}, {school_user}, {teacher_user});"""
+                                         VALUES (NOW(), NOW(), '{cla}', 'Ativo', {sch_id}, {school_user}, {teacher_user});"""
                 c = self.con.cursor()
                 c.execute(query)
                  
@@ -143,7 +150,6 @@ class ExecuteMock():
         for cla in classes:
             cla = cla['cla_id']
             for i in range(0, 40):
-                print (student_index)
                 student_user = student_users[student_index]['usr_id']
                 query = f"""INSERT INTO Attendances (createdAt, updatedAt, cla_id, student_user_id)
                                               VALUES (NOW(), NOW(), {cla}, {student_user});"""
@@ -152,8 +158,34 @@ class ExecuteMock():
                 c.execute(query) 
         
         c = self.con.cursor()
-        c.execute('COMMIT;')       
-    
+        c.execute('COMMIT;')
+        
+        # Insert Fluency Metrics
+        try:
+            with self.con.cursor() as c:
+                c.execute("SELECT sch_id, teacher_user_id, student_user_id FROM Classes, Attendances WHERE Classes.cla_id = Attendances.cla_id;")
+                students = c.fetchall()
+        finally:
+            c.close()
+        
+        for student in students:
+            metrics = []
+            month = 0
+            day = 0
+            for i in range(0, (len(self.days) * len(self.months))):
+                metrics.append({'idSchool': student['sch_id'], 
+                                'idProfessor': student['teacher_user_id'], 
+                                'idStudent': student['student_user_id'],
+                                'metrica': self.metrics[randint(0,2)], 
+                                'createdAt': datetime.now().replace(month=self.months[month], day=self.days[day])})  
+                if day == len(self.days)-1:
+                    day = 0
+                    month += 1
+                else:
+                    day += 1
+            self.fluencymetrics.insert_many(metrics)
+            
+               
 class Mock():
     """
         Author:  Aline Rodrigues
@@ -166,7 +198,8 @@ class Mock():
         self.database = 'alfabd'
         self.host = '127.0.0.1'
         self.user = 'root'
-        self.senha = ''
+        self.password = ''
+        self.mongo = ''
 
     def usage(self):
         debug('______________________________________\n')
@@ -176,8 +209,9 @@ class Mock():
         debug('-> Command: -i  (host or IP) default 127.0.0.1')
         debug('-> Command: -u  (user name) default root')
         debug('-> Command: -p  (password) required')
+        debug('-> Command: -m  (mongodb) required')
         debug('')
-        debug('-> Example: python mockMySQL.py -d name_database -i 127.0.0.1 -u root -p senha')
+        debug('-> Example: python mockMySQL.py -d name_database -i 127.0.0.1 -u root -p password, -m url_mongo')
         debug('')
 
         sys.exit()
@@ -191,7 +225,7 @@ class Mock():
             self.usage()
 
         try:
-            opts, args = getopt.getopt(args, "hd:i:u:p:", ["help", "database", "host", "user", "password"])
+            opts, args = getopt.getopt(args, 'hd:i:u:p:m:', ['help', 'database', 'host', 'user', 'password', 'mongo'])
         except getopt.GetoptError as err:
             debug(err)
             self.usage()
@@ -208,13 +242,17 @@ class Mock():
             elif o in ['-u', '--user']:
                 self.user = a
             elif o in ['-p', '--password']:
-                self.execute = True
-                self.senha = a
+                self.password = a
+            elif o in ['-m', '--mongo']:
+                self.mongo = a
             else:
-                assert False, "Unhandled Option"
+                assert False, 'Unhandled Option'
+        
+        if self.password != '' and self.mongo:
+            self.execute = True
 
         if self.execute:
-            ExecuteMock(self.database, self.host, self.user, self.senha).run()
+            ExecuteMock(self.database, self.host, self.user, self.password, self.mongo).run()
         else:
             self.usage()
 
