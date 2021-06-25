06deca8d261303c7fd15f71a210cb813f4379d17
1T Conte <contemmcm@fab.mil.br>
2021-06-25 09:24:07 -0300

    Criando volume para audios no conteiner Docker.

--- .deploy/update-deploy.sh
+++ .deploy/update-deploy.sh
@@ -27,8 +27,8 @@ elif [ $LOCAL = $BASE ]; then
     docker rm alfabd || true
 
     # Deploying new image
-    docker run -d -p 80:80 --restart unless-stopped --name=alfabd alfabd:develop
+    docker run -d --restart unless-stopped \
+        -p 80:80 -v /home/pc06/alfa-bd/audios:/app/audios \ 
+        --name=alfabd alfabd:develop
 
-    # Link to audio's folder 
-    ln -s /home/pc06/alfa-bd/audios /home/pc06/ibpl-alfa-bd-2021/ 
 fi
